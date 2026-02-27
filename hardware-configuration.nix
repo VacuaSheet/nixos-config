700387e434c2d51753e6c9543eb34842d05e6071
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-02-27 00:38:48 -0300

    vscode_atualização-unstable - 2026-02-27 00:38

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -5,7 +5,7 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/modulesPath")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
