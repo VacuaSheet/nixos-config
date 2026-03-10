13460fa5f77b9afe04aea97bce6d4bedf79e14da
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-03-10 13:13:56 -0300

    lutris_extensões - 2026-03-10 13:13

--- flatpak.nix
+++ flatpak.nix
@@ -20,7 +20,7 @@ in
       "org.mozilla.firefox"
       "io.gitlab.librewolf-community"
       "com.usebottles.bottles"
-      "net.lutris.Lutris"          # Lutris
+      "net.lutris.Lutris//stable"          # Lutris
        "org.freedesktop.Platform.Compat.i386//25.08"
        "org.freedesktop.Platform.GL32.default//25.08"
       "com.heroicgameslauncher.hgl" # Heroic Games Launcher
