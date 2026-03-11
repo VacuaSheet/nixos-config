40b94239301eea892143ccec88a3604d5a1bb7e6
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-03-11 10:13:05 -0300

    movendo o ollama para outro diretorio - 2026-03-11 10:13

--- configuration.nix
+++ configuration.nix
@@ -438,7 +438,6 @@ systemd.user.services.unmute-hardware-audio = {
   # Habilita o serviço do Ollama
    services.ollama = {
     enable = true;
-    user = "ollama"; # Garante que o serviço use o usuário dedicado
     acceleration = false; # Habilita aceleração para GPUs AMD (incluindo integradas)
       environmentVariables = {
     OLLAMA_ORIGINS = "*";
