f521811758084cb788c593cd11e85376d69ba02b
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-03-12 16:50:51 -0300

    go-anime - 2026-03-12 16:50

--- configuration.nix
+++ configuration.nix
@@ -359,7 +359,7 @@ programs.zsh = {
     usbutils # Para consultar e gerenciar barramentos e dispositivos USB no Linux
     direnv
      mpv # OBRIGATÓRIO: O GoAnime usa ele para dar o play
-    inputs.goanime.packages.${pkgs.system}.default # Go Anim
+   inputs.goanime.packages.${pkgs.stdenv.hostPlatform.system}.default # Go Anim
      go # Linguagem de programação
   ];
 
