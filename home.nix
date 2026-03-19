deba1682b70a48a2990c7c969041ec3a6aaee53c
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-03-18 22:36:13 -0300

    conf_lutris - 2026-03-18 22:36

--- home.nix
+++ home.nix
@@ -28,24 +28,23 @@ in
      gnutls
      wine-staging
      winetricks
-  (lutris.override {
-    extraPkgs = pkgs: [
-      gnutls
-      libgpg-error
-      freetype
-      sqlite
-      libxml2
-      libpulseaudio
-    ];
-  })
-  (pkgs.lutris.overrideAttrs (oldAttrs: {
-    src = pkgs.fetchFromGitHub {
-      owner = "lutris";
-      repo = "lutris";
-      rev = "master";
-      hash = "sha256-MC9RRE+VtMSzq9ro5nbxi/NSWsuYjQvX+9ARnEnzDdU=";
-    };
-  }))
+(pkgs.lutris.override {
+  extraPkgs = p: [
+    p.gnutls
+    p.libgpg-error
+    p.freetype
+    p.sqlite
+    p.libxml2
+    p.libpulseaudio
+  ];
+}).overrideAttrs (oldAttrs: {
+  src = pkgs.fetchFromGitHub {
+    owner = "lutris";
+    repo = "lutris";
+    rev = "master";
+    hash = "sha256-MC9RRE+VtMSzq9ro5nbxi/NSWsuYjQvX+9ARnEnzDdU=";
+  };
+})
   ];
 
      # 1. Habilita o dconf dentro do Home Manager
