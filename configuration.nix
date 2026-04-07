41c93f5b23634a633b4f0cd3f253a45d605402cc
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-04-07 00:58:30 -0300

    squidservers - 2026-04-07 00:58

--- configuration.nix
+++ configuration.nix
@@ -230,7 +230,7 @@ programs.zsh = {
         flat = "f() { flatpak remote-add --if-not-exists flathub https://dl.flathub.org && flatpak install flathub \"$1\"; }; f";
         goanime = "$HOME/go/bin/goanime";
        # Servidor Minecraft
-       squidservers = "bwrap --ro-bind /nix /nix --ro-bind /etc /etc --ro-bind /run/current-system /run/current-system --ro-bind /run/wrappers /run/wrappers --ro-bind /run/opengl-driver /run/opengl-driver --ro-bind /run/dbus /run/dbus --bind /run/user/1000 /run/user/1000 --dev-bind /dev /dev --setenv WAYLAND_DISPLAY wayland-0 --setenv XDG_RUNTIME_DIR /run/user/1000 --bind /home/_-_-yakov_-_-/Apps /home/_-_-yakov_-_-/Apps --bind /home/_-_-yakov_-_-/.cache /home/_-_-yakov_-_-/.cache --bind /home/_-_-yakov_-_-/.config /home/_-_-yakov_-_-/.config --proc /proc --tmpfs /tmp --share-net appimage-run /home/_-_-yakov_-_-/Apps/squidservers-latest.appimage -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing --disable-gpu-rasterization --disable-dev-shm-usage";
+       squidservers = "bwrap ... (comando longo) ... squidservers-latest.appimage > /dev/null 2>&1 &";
          };  
              
      # 2. A função para o sga aceitar argumentos (FORA do bloco acima)
