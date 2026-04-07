008dbb389f90127652e3b2fedbfda8c130da72ab
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-04-07 01:55:46 -0300

    squidservers - 2026-04-07 01:55

--- squidservers.nix
+++ squidservers.nix
@@ -22,7 +22,7 @@
         --tmpfs /tmp \
         --share-net \
         ${pkgs.appimage-run}/bin/appimage-run /home/_-_-yakov_-_-/Apps/SquidServersData/squidservers-latest.appimage \
-        -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing > /dev/null 2>&1 &
+        -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing 
     '')
 
     # 2. Atalho do Menu (Atualizado com o novo caminho do ícone)
