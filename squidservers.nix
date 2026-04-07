296a5dd815a0fb2ba0d7178f55480d703cdb9e5a
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-04-07 01:50:13 -0300

    squidservers - 2026-04-07 01:50

--- squidservers.nix
+++ squidservers.nix
@@ -21,8 +21,6 @@
         --proc /proc \
         --tmpfs /tmp \
         --share-net \
-        /* ${pkgs.appimage-run}/bin/appimage-run /home/_-_-yakov_-_-/Apps/SquidServersData/squidservers-latest.appimage \
-        -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing > /dev/null 2>&1 & */ # Isso coloca uma bolha dentro dia uma bolha não quer dizer mais seguro só mais lento        
         /home/_-_-yakov_-_-/Apps/SquidServersData/squidservers-latest.appimage -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing > /dev/null 2>&1 &
     '')
 
