47a53c19ac1e4156658c5d7515aeeb6224f46cc3
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-04-06 23:55:07 -0300

    -Wrap+bubblewrap - 2026-04-06 23:55

--- configuration.nix
+++ configuration.nix
@@ -395,7 +395,7 @@ programs.zsh = {
      gst_all_1.gst-libav # Isso resolve o erro do libavfilter
     anydesk # Gerenciador de outro pc
     appimage-run
-    wrap
+    bubblewrap
   ];
 
    # Configuração VS Code
