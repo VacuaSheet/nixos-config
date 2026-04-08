6ca01213a4882131f0abfdbfeeb47b23fdb33a8a
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-04-08 19:47:38 -0300

    squidservers - 2026-04-08 19:47

--- squidservers.nix
+++ squidservers.nix
@@ -1,44 +1,47 @@
 { pkgs, ... }:
 
 let
-  # Caminho para o ícone que você baixou
   iconFile = "/home/_-_-yakov_-_-/Apps/SquidServersData/squidservers.png";
+
+  # 1. Criamos o executável (o script bwrap)
+  meuScript = pkgs.writeShellScriptBin "squidservers" ''
+    ${pkgs.bubblewrap}/bin/bwrap \
+      --ro-bind /nix /nix \
+      --ro-bind /etc /etc \
+      --ro-bind /run/current-system /run/current-system \
+      --ro-bind /run/wrappers /run/wrappers \
+      --ro-bind /run/opengl-driver /run/opengl-driver \
+      --ro-bind /run/dbus /run/dbus \
+      --bind /run/user/1000 /run/user/1000 \
+      --dev-bind /dev /dev \
+      --setenv WAYLAND_DISPLAY wayland-0 \
+      --setenv XDG_RUNTIME_DIR /run/user/1000 \
+      --bind /home/_-_-yakov_-_-/Apps /home/_-_-yakov_-_-/Apps \
+      --bind /home/_-_-yakov_-_-/.cache /home/_-_-yakov_-_-/.cache \
+      --bind /home/_-_-yakov_-_-/.config /home/_-_-yakov_-_-/.config \
+      --proc /proc \
+      --tmpfs /tmp \
+      --share-net \
+      ${pkgs.appimage-run}/bin/appimage-run /home/_-_-yakov_-_-/Apps/SquidServersData/squidservers-latest.appimage \
+      -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing > /dev/null 2>&1 &
+  '';
+
+  # 2. Criamos o "pacote" do atalho
+  meuAtalho = pkgs.makeDesktopItem {
+    name = "squidservers";
+    desktopName = "SquidServers";
+    genericName = "Servidor de Minecraft";
+    exec = "squidservers"; # Aqui ele chama o nome do script acima
+    icon = iconFile;
+    categories = [ "Game" ];
+    terminal = false;
+  };
+
 in
 {
-  environment.systemPackages = with pkgs; [
-    # 1. O script que já criamos (o comando do terminal)
-    (pkgs.writeShellScriptBin "squidservers" ''
-      ${pkgs.bubblewrap}/bin/bwrap \
-        --ro-bind /nix /nix \
-        --ro-bind /etc /etc \
-        --ro-bind /run/current-system /run/current-system \
-        --ro-bind /run/wrappers /run/wrappers \
-        --ro-bind /run/opengl-driver /run/opengl-driver \
-        --ro-bind /run/dbus /run/dbus \
-        --bind /run/user/1000 /run/user/1000 \
-        --dev-bind /dev /dev \
-        --setenv WAYLAND_DISPLAY wayland-0 \
-        --setenv XDG_RUNTIME_DIR /run/user/1000 \
-        --bind /home/_-_-yakov_-_-/Apps /home/_-_-yakov_-_-/Apps \
-        --bind /home/_-_-yakov_-_-/.cache /home/_-_-yakov_-_-/.cache \
-        --bind /home/_-_-yakov_-_-/.config /home/_-_-yakov_-_-/.config \
-        --proc /proc \
-        --tmpfs /tmp \
-        --share-net \
-        ${pkgs.appimage-run}/bin/appimage-run /home/_-_-yakov_-_-/Apps/SquidServersData/squidservers-latest.appimage \
-        -- --ozone-platform=wayland --ignore-gpu-blocklist --enable-gpu-rasterization --enable-zero-copy --disable-gpu-compositing > /dev/null 2>&1 &
-    '')
-
-    # 2. O atalho para o Menu de Aplicativos (com o ícone!)
-    (pkgs.makeDesktopItem {
-      name = "squidservers";
-      desktopName = "SquidServers";
-	      genericName = "Servidor de Minecraft";
-      exec = "squidservers";
-      icon = iconFile;
-      comment = "Gerenciador de Servidores Minecraft";
-      categories = [ "Game" ];
-      terminal = false;
-    })
+  # 3. Adicionamos AMBOS ao sistema
+  environment.systemPackages = [
+    meuScript
+    meuAtalho
   ];
 }
