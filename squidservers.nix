422d95f90e8349625d6886bcd9f7e3faa620e809
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-04-12 20:42:57 -0300

    squidservers - 2026-04-12 20:42

--- squidservers.nix
+++ squidservers.nix
@@ -9,6 +9,9 @@ let
     paths = [
       # 1. O Script executável
       (pkgs.writeShellScriptBin "squidservers" ''
+	export NIX_LD=$(cat ${pkgs.stdenv.cc}/nix-support/dynamic-linker)
+        export NIX_LD_LIBRARY_PATH=/run/current-system/sw/share/nix-ld/lib
+
         ${pkgs.bubblewrap}/bin/bwrap \
           --ro-bind /nix /nix \
           --ro-bind /etc /etc \
@@ -18,6 +21,9 @@ let
           --ro-bind /run/dbus /run/dbus \
           --bind /run/user/1000 /run/user/1000 \
           --dev-bind /dev /dev \
+          --ro-bind /run/current-system/sw/share/nix-ld/lib /run/current-system/sw/share/nix-ld/lib \
+          --setenv NIX_LD_LIBRARY_PATH "$NIX_LD_LIBRARY_PATH" \
+          --setenv NIX_LD "$NIX_LD" \
           --setenv WAYLAND_DISPLAY wayland-0 \
           --setenv XDG_RUNTIME_DIR /run/user/1000 \
           --bind /home/_-_-yakov_-_-/Apps /home/_-_-yakov_-_-/Apps \
@@ -27,7 +33,7 @@ let
           --tmpfs /tmp \
           --share-net \
           ${pkgs.appimage-run}/bin/appimage-run /home/_-_-yakov_-_-/Apps/SquidServersData/squidservers-latest.appimage \
-          -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing > /dev/null 2>&1 &
+          -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing 
       '')
 
       # 2. O Atalho do Desktop
