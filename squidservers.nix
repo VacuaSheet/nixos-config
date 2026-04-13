8ebfc9d26a87693ec6579af7183e3d3bc51fceaa
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-04-12 22:37:36 -0300

    squidservers - 2026-04-12 22:37

--- squidservers.nix
+++ squidservers.nix
@@ -1,32 +1,17 @@
-{ pkgs, ... }:
-
-let
-  iconFile = "/home/_-_-yakov_-_-/Apps/SquidServersData/squidservers.png";
-
-  # Criamos um pacote único que contém TUDO
-  squidPackage = pkgs.symlinkJoin {
-    name = "squidservers-app";
-    paths = [
-      # 1. O Script executável
       (pkgs.writeShellScriptBin "squidservers" ''
+        export NIX_LD=$(cat ${pkgs.stdenv.cc}/nix-support/dynamic-linker)
         export NIX_LD_LIBRARY_PATH="/run/current-system/sw/share/nix-ld/lib"
-	export NIX_LD=$(cat ${pkgs.stdenv.cc}/nix-support/dynamic-linker)
-        export NIX_LD_LIBRARY_PATH=/run/current-system/sw/share/nix-ld/lib
 
         ${pkgs.bubblewrap}/bin/bwrap \
           --ro-bind /nix /nix \
           --ro-bind /etc /etc \
-	  --ro-bind /run/current-system/sw /run/current-system/sw \
+          --ro-bind /run /run \
+          --ro-bind /usr /usr \
           --symlink /run/current-system/sw/lib64 /lib64 \
-          --ro-bind /run/current-system /run/current-system \
-          --ro-bind /run/wrappers /run/wrappers \
-          --ro-bind /run/opengl-driver /run/opengl-driver \
-          --ro-bind /run/dbus /run/dbus \
           --bind /run/user/1000 /run/user/1000 \
           --dev-bind /dev /dev \
-          --ro-bind /run/current-system/sw/share/nix-ld/lib /run/current-system/sw/share/nix-ld/lib \
-          --setenv NIX_LD_LIBRARY_PATH "$NIX_LD_LIBRARY_PATH" \
           --setenv NIX_LD "$NIX_LD" \
+          --setenv NIX_LD_LIBRARY_PATH "$NIX_LD_LIBRARY_PATH" \
           --setenv WAYLAND_DISPLAY wayland-0 \
           --setenv XDG_RUNTIME_DIR /run/user/1000 \
           --bind /home/_-_-yakov_-_-/Apps /home/_-_-yakov_-_-/Apps \
@@ -38,25 +23,3 @@ let
           ${pkgs.appimage-run}/bin/appimage-run /home/_-_-yakov_-_-/Apps/SquidServersData/squidservers-latest.appimage \
           -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing 
       '')
-
-      # 2. O Atalho do Desktop
-      (pkgs.makeDesktopItem {
-        name = "squidservers";
-        desktopName = "SquidServers";
-        genericName = "Servidor de Minecraft";
-        exec = "squidservers";
-        icon = "${iconFile}";
-        categories = [ "Game" "ActionGame" ];
-	keywords = [ "minecraft" "server" "squid" ];
-        terminal = false;
-      })
-    ];
-  };
-
-in
-{
-  # Agora instalamos apenas o pacote unificado
-  environment.systemPackages = [
-    squidPackage
-  ];
-}
