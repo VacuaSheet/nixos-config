429b417c5a5f6786919f6939c27917d4cf86474d
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-04-12 22:38:14 -0300

    squidservers - 2026-04-12 22:38

--- squidservers.nix
+++ squidservers.nix
@@ -1,3 +1,13 @@
+{ pkgs, ... }:
+
+let
+  iconFile = "/home/_-_-yakov_-_-/Apps/SquidServersData/squidservers.png";
+
+  # Criamos o pacote unificado com o script e o atalho
+  squidPackage = pkgs.symlinkJoin {
+    name = "squidservers-app";
+    paths = [
+      # 1. O Script executável corrigido
       (pkgs.writeShellScriptBin "squidservers" ''
         export NIX_LD=$(cat ${pkgs.stdenv.cc}/nix-support/dynamic-linker)
         export NIX_LD_LIBRARY_PATH="/run/current-system/sw/share/nix-ld/lib"
@@ -21,5 +31,25 @@
           --tmpfs /tmp \
           --share-net \
           ${pkgs.appimage-run}/bin/appimage-run /home/_-_-yakov_-_-/Apps/SquidServersData/squidservers-latest.appimage \
-          -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing 
+          -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing
       '')
+
+      # 2. O Atalho do Desktop
+      (pkgs.makeDesktopItem {
+        name = "squidservers";
+        desktopName = "SquidServers";
+        genericName = "Servidor de Minecraft";
+        exec = "squidservers";
+        icon = "${iconFile}";
+        categories = [ "Game" "ActionGame" ];
+        keywords = [ "minecraft" "server" "squid" ];
+        terminal = false;
+      })
+    ];
+  };
+in
+{
+  environment.systemPackages = [
+    squidPackage
+  ];
+}
