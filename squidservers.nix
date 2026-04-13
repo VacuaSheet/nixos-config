8d3e5d1f4301453f305e516d6b636ac15b2410a8
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-04-12 22:54:12 -0300

    squidservers - 2026-04-12 22:54

--- squidservers.nix
+++ squidservers.nix
@@ -3,16 +3,15 @@
 let
   iconFile = "/home/_-_-yakov_-_-/Apps/SquidServersData/squidservers.png";
 
-  # Criamos o pacote unificado com o script e o atalho
   squidPackage = pkgs.symlinkJoin {
     name = "squidservers-app";
     paths = [
+      # Adicionamos utilitários que o AppImage pode precisar para extrair o servidor
+      pkgs.unzip
+      pkgs.curl
+      pkgs.xz
 
-    pkgs.unzip
-    pkgs.gnutar
-    pkgs.curl
-    pkgs.xz
-      # 1. O Script executável corrigido
+      # O Script executável
       (pkgs.writeShellScriptBin "squidservers" ''
         export NIX_LD=$(cat ${pkgs.stdenv.cc}/nix-support/dynamic-linker)
         export NIX_LD_LIBRARY_PATH="/run/current-system/sw/share/nix-ld/lib"
@@ -22,17 +21,14 @@ let
           --ro-bind /etc /etc \
           --ro-bind /run /run \
           --ro-bind /usr /usr \
-          --ro-bind /etc/ssl/certs /etc/ssl/certs
+          --dev-bind /dev /dev \
           --symlink /run/current-system/sw/lib64 /lib64 \
           --bind /run/user/1000 /run/user/1000 \
-          --dev-bind /dev /dev \
           --setenv NIX_LD "$NIX_LD" \
           --setenv NIX_LD_LIBRARY_PATH "$NIX_LD_LIBRARY_PATH" \
           --setenv WAYLAND_DISPLAY wayland-0 \
           --setenv XDG_RUNTIME_DIR /run/user/1000 \
-          --bind /home/_-_-yakov_-_-/Apps /home/_-_-yakov_-_-/Apps \
-          --bind /home/_-_-yakov_-_-/.cache /home/_-_-yakov_-_-/.cache \
-          --bind /home/_-_-yakov_-_-/.config /home/_-_-yakov_-_-/.config \
+          --bind /home/_-_-yakov_-_- /home/_-_-yakov_-_- \
           --proc /proc \
           --tmpfs /tmp \
           --share-net \
@@ -40,22 +36,18 @@ let
           -- --ozone-platform=wayland --disable-gpu --disable-gpu-compositing
       '')
 
-      # 2. O Atalho do Desktop
       (pkgs.makeDesktopItem {
         name = "squidservers";
         desktopName = "SquidServers";
         genericName = "Servidor de Minecraft";
         exec = "squidservers";
         icon = "${iconFile}";
-        categories = [ "Game" "ActionGame" ];
-        keywords = [ "minecraft" "server" "squid" ];
+        categories = [ "Game" ];
         terminal = false;
       })
     ];
   };
 in
 {
-  environment.systemPackages = [
-    squidPackage
-  ];
+  environment.systemPackages = [ squidPackage ];
 }
