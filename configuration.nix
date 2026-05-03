89905385978ac65b92315506169c93a58d673cb7
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-05-03 12:04:49 -0300

    otimização de boot - 2026-05-03 12:04

--- configuration.nix
+++ configuration.nix
@@ -27,7 +27,12 @@ in
   boot.initrd.kernelModules = [ "amdgpu" ];
   # Isso garante que os módulos de virtualização sejam carregados
   boot.kernelModules = [ "kvm-amd" "ledtrig-disk" "ledtrig-activity" "leds-pci-amd" "i2c-dev" ]; # ou "kvm-intel" se seu processador for Intel
-  
+  # Remove a espera pelas portas seriais inúteis que tem mais de 30 anos (Ganha ~5 segundos de timeout)  
+  boot.kernelParams = [ "8250.nr_uarts=0" ];
+  # Ignora a espera pela rede para abrir o login (Opcional, acelera o carregamento do Plasma)
+  systemd.services.NetworkManager-wait-online.enable = false;
+  # Otimização do Initrd (Ganha tempo no carregamento inicial)
+  boot.initrd.systemd.enable = true; # Usa systemd no initrd para maior paralelismo
 
   # Gaming: Gamemode & Steam
   programs.gamemode.enable = true;
