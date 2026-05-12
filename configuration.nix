cd45f6788f267816c77cb6542ed6305dd1439f9e
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-05-12 15:05:14 -0300

    R_erro_teclado - 2026-05-12 15:05

--- configuration.nix
+++ configuration.nix
@@ -23,11 +23,11 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   # "boot.kernelParams"  Remove a espera pelas portas seriais inúteis que tem mais de 30 anos (Ganha ~5 segundos de timeout)
-  boot.kernelParams = [ "amd_pstate=active" "8250.nr_uarts=0" ]; #"amd_pstate=active" Ativa o controle fino de energia/clocks 
+  boot.kernelParams = [ "amd_pstate=active" "8250.nr_uarts=0" "usbcore.autosuspend=-1" ]; #"amd_pstate=active" Ativa o controle fino de energia/clocks 
   # Garante que o driver carregue cedo no boot para evitar problemas de resolução
   boot.initrd.kernelModules = [ "amdgpu" ];
   # Isso garante que os módulos de virtualização sejam carregados
-  boot.kernelModules = [ "kvm-amd" "ledtrig-disk" "ledtrig-activity" "leds-pci-amd" "i2c-dev" ]; # ou "kvm-intel" se seu processador for Intel  
+  boot.kernelModules = [ "kvm-amd" "i2c-dev" ]; # ou "kvm-intel" se seu processador for Intel  
   # Ignora a espera pela rede para abrir o login (Opcional, acelera o carregamento do Plasma)
   systemd.services.NetworkManager-wait-online.enable = false;
   # Reduz o tempo de espera no menu do NixOS 
