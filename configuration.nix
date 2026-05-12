0b51cd4cfb043d8aa7c664d257ab78c43f6ee27b
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-05-12 16:17:58 -0300

    R_erro_teclado - 2026-05-12 16:17

--- configuration.nix
+++ configuration.nix
@@ -501,10 +501,6 @@ programs.zsh = {
       package = pkgs.openrgb;
     };
 
-  # Garante que as regras udev de periféricos RGB sejam carregadas no boot
-    hardware.services.openrgb.enable = true; # (Se sua versão do NixOS exigir essa flag extra)
-
-
   # Processo persistente em segundo plano que força a ativação física das luzes do teclado:
     systemd.user.services.fix-keyboard-leds = {
   description = "Forçar sincronização de LEDs do teclado no Wayland";
