1e419930b12f0f91d075151b8333182bdd800d9b
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-06-09 12:14:14 -0300

    +Faugus Launcher - 2026-06-09 12:14

--- flatpak.nix
+++ flatpak.nix
@@ -31,6 +31,7 @@ in
       "org.freedesktop.Platform.GL32.default//25.08"
       "org.freedesktop.Platform.VulkanLayer.gamescope//25.08"
       "io.github.rfrench3.scopebuddy-gui" #automatiza o gamescope
+      "io.github.Faugus.faugus-launcher" #laucher com mods e mais
     ];
 
   overrides = {
