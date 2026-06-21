b714b15dfcaa89afe3462e8b27842b78afb361fb
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-06-21 18:38:57 -0300

    +atalhos - 2026-06-21 18:38

--- configuration.nix
+++ configuration.nix
@@ -248,6 +248,9 @@ programs.zsh = {
         c = "printf \"\\033[2J\\033[3J\\033[1;1H\"" ;
         flat = "f() { flatpak remote-add --if-not-exists flathub https://dl.flathub.org && flatpak install flathub \"$1\"; }; f";
         goanime = "$HOME/go/bin/goanime";
+        zce = "nano ~/.zsh_history"
+        zcp = "LC_ALL=C sed -i '/TERMO_QUE_VOCE_QUER_APAGAR/d' ~/.zsh_history"
+        zch = "echo -n "" > ~/.zsh_history"
          };  
              
      # 2. A função para o sga aceitar argumentos (FORA do bloco acima)
