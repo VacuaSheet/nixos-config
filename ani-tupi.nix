509876f873adba43cbe4c750adc93fdc591d3546
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-06-24 12:11:16 -0300

    +ani-tupi - 2026-06-24 12:11

--- ani-tupi.nix
+++ ani-tupi.nix
@@ -1,69 +1,57 @@
 { pkgs, ... }:
 
 let
-  # 1. Baixa o repositório oficial em Python do levyvix
-  ani-tupi-src = pkgs.fetchFromGitHub {
+  # 1. Baixamos o repositório em Python atualizado do levyvix
+  # Alteramos o identificador 'name' para forçar o Nix a baixar a pasta certa e ignorar o cache antigo
+  ani-tupi-repo = pkgs.fetchFromGitHub {
+    name = "ani-tupi-python-src";
     owner = "levyvix";
     repo = "ani-tupi";
     rev = "master";
-    # Hash do repositório Python atualizado
-    hash = "sha256-pWrPuzpilXwHVRLwJB+XMkgTiyPRIKdoMf4fXS8GnPg="; 
+    hash = "sha256-pWrPuzpilXwHVRLwJB+XMkgTiyPRIKdoMf4fXS8GnPg=";
   };
 
-  # 2. Empacota a aplicação Python nativamente no ecossistema Nix
-  ani-tupi-app = pkgs.python3Packages.buildPythonApplication {
-    pname = "ani-tupi";
-    version = "1.2.1";
-    src = ani-tupi-src;
-    format = "pyproject";
-
-    # Dependências do ecossistema Python que o programa usa para rodar
-    propagatedBuildInputs = with pkgs.python3Packages; [
-      setuptools
-      requests
-      beautifulsoup4
-      prompt-toolkit
-    ];
-
-    doCheck = false;
-  };
-
-  # 3. Cria a Sandbox do Bubblewrap trancando o executável Python
-  ani-tupi-sandbox = pkgs.writeShellApplication {
-    name = "ani-tupi";
-    
-    runtimeInputs = with pkgs; [ 
-      mpv          # Player de vídeo obrigatório
-      zathura      # Leitor de PDF para Mangá (recomendado)
-      curl 
-      gnugrep 
-      coreutils 
-      bubblewrap 
-    ];
-
-    text = ''
-      exec bwrap \
-        --ro-bind /nix/store /nix/store \
-        --dev /dev \
-        --proc /proc \
-        --tmpfs /tmp \
-        --share-net \
-        --die-with-parent \
-        --bind-try "$HOME/.config/ani-tupi" "$HOME/.config/ani-tupi" \
-        --bind-try "$HOME/.config/mpv" "$HOME/.config/mpv" \
-        --ro-bind-try "/run/user/$(id -u)/pulse" "/run/user/$(id -u)/pulse" \
-        --ro-bind-try "/run/user/$(id -u)/pipewire-0" "/run/user/$(id -u)/pipewire-0" \
-        --ro-bind-try "/run/user/$(id -u)/wayland-0" "/run/user/$(id -u)/wayland-0" \
-        --setenv DISPLAY "$DISPLAY" \
-        --setenv WAYLAND_DISPLAY "$WAYLAND_DISPLAY" \
-        --setenv XDG_RUNTIME_DIR "/run/user/$(id -u)" \
-        "${ani-tupi-app}/bin/ani-tupi" "$@"
-    '';
-  };
+  # 2. Montamos o ambiente Python do Nix com todas as dependências que o script pede
+  python-env = pkgs.python3.withPackages (ps: with ps; [
+    requests
+    beautifulsoup4
+    prompt-toolkit
+    setuptools
+  ]);
 in
 {
-  # 4. Injeta o executável final na sua Home
+  # 3. Criamos a Sandbox do Bubblewrap injetando o ambiente Python estruturado
   home.packages = [
-    ani-tupi-sandbox
+    (pkgs.writeShellApplication {
+      name = "ani-tupi";
+      
+      runtimeInputs = with pkgs; [ 
+        mpv          # Player de vídeo obrigatório
+        zathura      # Leitor de PDF para Mangá (recomendado)
+        curl 
+        gnugrep 
+        coreutils 
+        bubblewrap 
+      ];
+
+      text = ''
+        exec bwrap \
+          --ro-bind /nix/store /nix/store \
+          --dev /dev \
+          --proc /proc \
+          --tmpfs /tmp \
+          --share-net \
+          --die-with-parent \
+          --bind-try "$HOME/.config/ani-tupi" "$HOME/.config/ani-tupi" \
+          --bind-try "$HOME/.config/mpv" "$HOME/.config/mpv" \
+          --ro-bind-try "/run/user/$(id -u)/pulse" "/run/user/$(id -u)/pulse" \
+          --ro-bind-try "/run/user/$(id -u)/pipewire-0" "/run/user/$(id -u)/pipewire-0" \
+          --ro-bind-try "/run/user/$(id -u)/wayland-0" "/run/user/$(id -u)/wayland-0" \
+          --setenv DISPLAY "$DISPLAY" \
+          --setenv WAYLAND_DISPLAY "$WAYLAND_DISPLAY" \
+          --setenv XDG_RUNTIME_DIR "/run/user/$(id -u)" \
+          "${python-env}/bin/python" "${ani-tupi-repo}/main.py" "$@"
+      '';
+    })
   ];
 }
