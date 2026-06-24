4c6105f1fdac0a38e3dc1536171c12aade2a62d0
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-06-24 11:25:24 -0300

    +atalhos - 2026-06-24 11:25

--- configuration.nix
+++ configuration.nix
@@ -248,7 +248,10 @@ programs.zsh = {
         c = "printf \"\\033[2J\\033[3J\\033[1;1H\"" ;
         flat = "f() { flatpak remote-add --if-not-exists flathub https://dl.flathub.org && flatpak install flathub \"$1\"; }; f";
         goanime = "$HOME/go/bin/goanime";  
-	ani-tupi = "nix shell nixpkgs#uv nixpkgs#mpv nixpkgs#fzf nixpkgs#curl nixpkgs#yt-dlp nixpkgs#webkitgtk_4_1 -c sh -c 'env WEBKIT_DISABLE_COMPOSITING_MODE=1 STEAM_APPID=\"\" SB_APPID=\"\" PLAYER=mpv uv run --script https://githubusercontent.com'";
+	goanime = "$HOME/go/bin/goanime";
+        ani-tupi = "nix shell nixpkgs#uv nixpkgs#mpv nixpkgs#fzf nixpkgs#curl nixpkgs#yt-dlp nixpkgs#webkitgtk_4_1 -c sh -c 'env WEBKIT_DISABLE_COMPOSITING_MODE=1 STEAM_APPID=\"\" SB_APPID=\"\" PLAYER=mpv uv run --project https://github.com main.py'";
+        zce = "nano ~/.zsh_history";
+
  	zce = "nano ~/.zsh_history";
 	zcp = "LC_ALL=C sed -i '/TERMO_QUE_VOCE_QUER_APAGAR/d' ~/.zsh_history";
 	zch = "echo -n \"\" > ~/.zsh_history";
