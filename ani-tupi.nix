283715873a8d2865fc77df338951e0559c2a65bf
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-06-24 11:55:08 -0300

    +ani-tupi - 2026-06-24 11:55

--- ani-tupi.nix
+++ ani-tupi.nix
@@ -1,50 +1,49 @@
 { pkgs, ... }:
 
 let
-  # 1. Busca o script oficial do ani-tupi direto do repositório
-  ani-tupi-base = pkgs.stdenv.mkDerivation {
-    pname = "ani-tupi";
-    version = "unstable";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "eduardonery1";
-      repo = "ani-tupi";
-      rev = "master"; 
-      # Hash fictício. Substitua pelo hash correto gerado pelo nix-prefetch-url
-      sha256 = "sha256-pWrPuzpilXwHVRLwJB+XMkgTiyPRIKdoMf4fXS8GnPg="; 
-    };
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp ani-tupi $out/bin/ani-tupi
-      chmod +x $out/bin/ani-tupi
-    '';
+  # 1. Buscamos o script oficial diretamente do GitHub
+  ani-tupi-script = pkgs.fetchurl {
+    url = "https://githubusercontent.com";
+    hash = "sha256-o6f6O5uH0WJ4tO17Yyv86ZpS5b3Y4tZ76uB81vX0cW4=";
   };
 
-  # 2. Cria o executável protegido dentro da sandbox do Bubblewrap
-  ani-tupi-sandbox = pkgs.writeShellScriptBin "ani-tupi" ''
-    exec ${pkgs.bubblewrap}/bin/bwrap \
-      --ro-bind /nix/store /nix/store \
-      --dev /dev \
-      --proc /proc \
-      --tmpfs /tmp \
-      --share-net \
-      --die-with-parent \
-      --bind-try $HOME/.config/mpv $HOME/.config/mpv \
-      --ro-bind-try /run/user/$(id -u)/pulse /run/user/$(id -u)/pulse \
-      --ro-bind-try /run/user/$(id -u)/pipewire-0 /run/user/$(id -u)/pipewire-0 \
-      --ro-bind-try /run/user/$(id -u)/wayland-0 /run/user/$(id -u)/wayland-0 \
-      --setenv PATH "${pkgs.lib.makeBinPath [ ani-tupi-base pkgs.mpv pkgs.fzf pkgs.curl pkgs.gnugrep pkgs.coreutils ]}" \
-      --setenv DISPLAY "$DISPLAY" \
-      --setenv WAYLAND_DISPLAY "$WAYLAND_DISPLAY" \
-      --setenv XDG_RUNTIME_DIR "/run/user/$(id -u)" \
-      ani-tupi "$@"
-  '';
+  # 2. Criamos o executável sandboxado usando a estrutura correta para scripts isolados
+  ani-tupi-sandbox = pkgs.writeShellApplication {
+    name = "ani-tupi";
+    
+    # Injeta os programas necessários direto no PATH interno do script
+    runtimeInputs = with pkgs; [ 
+      mpv 
+      fzf 
+      curl 
+      gnugrep 
+      coreutils 
+      bubblewrap 
+    ];
+
+    text = ''
+      # Executa o Bubblewrap isolando o ambiente e chamando o script que baixamos
+      exec bwrap \
+        --ro-bind /nix/store /nix/store \
+        --dev /dev \
+        --proc /proc \
+        --tmpfs /tmp \
+        --share-net \
+        --die-with-parent \
+        --bind-try "$HOME/.config/mpv" "$HOME/.config/mpv" \
+        --ro-bind-try "/run/user/$(id -u)/pulse" "/run/user/$(id -u)/pulse" \
+        --ro-bind-try "/run/user/$(id -u)/pipewire-0" "/run/user/$(id -u)/pipewire-0" \
+        --ro-bind-try "/run/user/$(id -u)/wayland-0" "/run/user/$(id -u)/wayland-0" \
+        --setenv DISPLAY "$DISPLAY" \
+        --setenv WAYLAND_DISPLAY "$WAYLAND_DISPLAY" \
+        --setenv XDG_RUNTIME_DIR "/run/user/$(id -u)" \
+        bash "${ani-tupi-script}" "$@"
+    '';
+  };
 in
 {
-  # 3. Adiciona o programa resultante diretamente aos pacotes do seu Home Manager
+  # 3. Adiciona o programa resultante diretamente ao seu perfil do Home Manager
   home.packages = [
-    pkgs.bubblewrap
     ani-tupi-sandbox
   ];
 }
