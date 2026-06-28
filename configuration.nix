a07b27f7c113bd84085a4a3ef62ff28daba57a52
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-06-28 19:41:00 -0300

    waydroid ~ - 2026-06-28 19:41

--- configuration.nix
+++ configuration.nix
@@ -392,7 +392,7 @@ comitav() {
   alsa-utils # APP mantedor do unmute
   alsa-tools 
   steamtinkerlaunch
-  waydroid
+ #  waydroid
   waydroid-helper
   polonium # multi janelas
   bubblewrap # Caixa de areia
