c7af5c66dbfeea9d65d749fd4d439521d51b5400
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-09-14 11:46:43 -0300

    sistema_de_25_para_26 - 2026-09-14 11:46

--- configuration.nix
+++ configuration.nix
@@ -499,7 +499,7 @@ comitav() {
   steamtinkerlaunch
   #waydroid
   #waydroid-helper
-  kdePackages.kwin-tiling # multi janelas
+  # kdePackages.kwin-tiling # multi janelas
   bubblewrap # Caixa de areia
 
          # Antvirus
