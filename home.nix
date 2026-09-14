ab4cac86ce3ff3727b13abe7225913c6a773ccfc
VacuaSheet <97613322+VacuaSheet@users.noreply.github.com>
2026-09-14 16:15:20 -0300

    temas - 2026-09-14 16:15

--- home.nix
+++ home.nix
@@ -36,6 +36,8 @@ in
     crudini
   ];
 
+	home.file.".local/share/plasma/look-and-feel/Dream-Dark-Color-Global-6".source = ./temas/Dream-Dark-Color-Global-6;
+
   home.activation.configure-kde-numlock = lib.hm.dag.entryAfter ["writeBoundary"] ''
     # Força a adição da linha do Num Lock direto no arquivo do painel se ela não existir
     if [ -f ~/.config/plasma-org.kde.plasma.desktop-appletsrc ]; then
